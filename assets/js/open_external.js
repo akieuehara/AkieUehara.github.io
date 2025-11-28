0941b4d3d83b27c7a969a9a4f3a518d7ca25b93b
akieuehara <uehara_a@cs.u-ryukyu.ac.jp>
2025-11-28 15:13:03 +0900

    Create open_external.js

---
+++ assets/js/open_external.js
@@ -0,0 +1,9 @@
+// すべての外部リンクを新しいタブで開く
+document.addEventListener("DOMContentLoaded", function () {
+    const links = document.querySelectorAll('a[href^="http"]');
+
+    links.forEach(link => {
+        link.setAttribute("target", "_blank");
+        link.setAttribute("rel", "noopener noreferrer");
+    });
+});
