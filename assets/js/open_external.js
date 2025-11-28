b55014f69c669c38a78643456bb0f7456b779e43
akieuehara <uehara_a@cs.u-ryukyu.ac.jp>
2025-11-28 15:36:49 +0900

    Update open_external.js

--- assets/js/open_external.js
+++ assets/js/open_external.js
@@ -1,9 +1,18 @@
-// すべての外部リンクを新しいタブで開く
 document.addEventListener("DOMContentLoaded", function () {
-    const links = document.querySelectorAll('a[href^="http"]');
+  const links = document.querySelectorAll("a");
 
-    links.forEach(link => {
-        link.setAttribute("target", "_blank");
-        link.setAttribute("rel", "noopener noreferrer");
-    });
+  links.forEach(link => {
+    const url = link.getAttribute("href");
+
+    // 外部リンク（http または https）
+    if (url.startsWith("http")) {
+      link.setAttribute("target", "_blank");
+      link.setAttribute("rel", "noopener noreferrer");
+    }
+
+    // 内部の markdown / html も新しいタブで開く
+    if (url.endsWith(".md") || url.endsWith(".html")) {
+      link.setAttribute("target", "_blank");
+    }
+  });
 });
